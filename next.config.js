0bcfcc8e08fe1bec31197077851d3a33154f830d
Jonathan Kevin <124524511+F34RL35S0N3@users.noreply.github.com>
2026-04-30 11:03:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Ganti 'nama-repo-anda' dengan nama repositori GitHub Anda yang sekarang
+  basePath: '/nama-repo-anda', 
 }
 
 module.exports = nextConfig
